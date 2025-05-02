f8844eac2cf29ae6b48e9295a7100cbd0d062ba7
王林 <1570898673@qq.com>
2025-05-02 18:03:40 +0800

    feat(travelogue): 添加游记功能模块
    - 创建游记 DTO 和实体模型
    - 实现游记创建、查询、更新和删除功能
    - 添加用户与游记的关联关系
    - 为游记相关操作添加权限控制

--- back/src/travelogue/entities/travelogue.entity.ts
+++ back/src/travelogue/entities/travelogue.entity.ts
@@ -1,1 +1,77 @@
-export class Travelogue {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+
+@Entity('travelogue')
+export class TravelogueEntity {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'travelogue_id' })
+  travelogueId: number; // 游记ID
+
+  // 用户ID
+  @Column({
+    type: 'int',
+    name: 'travelogue_user_id',
+  })
+  userId: number;
+
+  // 游记标题
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: false,
+    name: 'travelogue_title',
+  })
+  travelogueTitle: string;
+
+  // 游记内容
+  @Column({
+    type: 'text',
+    nullable: false,
+    name: 'travelogue_content',
+  })
+  travelogueContent: string;
+
+  // 创建日期
+  @CreateDateColumn({
+    type: 'datetime',
+    name: 'create_time',
+    nullable: false,
+  })
+  createTime: Date;
+
+  // 更新日期
+  @UpdateDateColumn({
+    type: 'datetime',
+    name: 'update_time',
+    nullable: false,
+  })
+  updateTime: Date;
+
+  // 游记状态
+  @Column({
+    type: 'int',
+    default: 0, // 0: 待审核，1: 通过，-1: 未通过
+    name: 'travelogue_status',
+  })
+  travelogueStatus: number;
+
+  // 游记封面
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    name: 'travelogue_cover',
+  })
+  travelogueCover: string;
+
+  // 关联用户实体，一个用户可以有多篇游记
+  @ManyToOne(() => UserEntity, (user) => user.travelogues, { nullable: false })
+  @JoinColumn({ name: 'travelogue_user_id' }) // 外键列名
+  user: UserEntity;
+}
--- back/src/travelogue/travelogue.module.ts
+++ back/src/travelogue/travelogue.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TravelogueService } from './travelogue.service';
 import { TravelogueController } from './travelogue.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TravelogueEntity } from './entities/travelogue.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TravelogueEntity, UserEntity])],
   controllers: [TravelogueController],
   providers: [TravelogueService],
 })
