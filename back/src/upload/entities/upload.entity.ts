7410cc5d300aeb4ac5ce74899379ae156d4a6e41
王林 <1570898673@qq.com>
2025-05-02 16:49:00 +0800

    feat(upload): 实现文件上传功能
    - 新增 Upload 实体，用于存储上传文件的信息
    - 实现单文件和多文件上传功能
    - 添加文件存储逻辑，支持哈希值计算和文件名生成
    - 集成 ServeStaticModule 以支持静态文件服务
    - 实现分页获取上传文件列表的功能
    - 删除了未使用的 DTO 类

--- back/src/upload/entities/upload.entity.ts
+++ back/src/upload/entities/upload.entity.ts
@@ -1,1 +1,24 @@
-export class Upload {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('uploads')
+export class Upload {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  hash: string;
+
+  @Column()
+  key: string;
+
+  @Column()
+  url: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
--- back/src/upload/upload.module.ts
+++ back/src/upload/upload.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Upload } from './entities/upload.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Upload]),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [UploadController],
   providers: [UploadService],
 })
