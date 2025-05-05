6bd649dd6bbf90ab4c1fa0ce007d7cfd4a81f6b8
HG-abab <1695059005@qq.com>
2025-05-05 18:36:02 +0800

    feat:首页功能

--- myApp/src/api/travelogue.js
+++ myApp/src/api/travelogue.js
@@ -1,9 +1,21 @@
 import { get } from "../utils/request";
 
 // 获取所有游记
-export const getTravelogs = () => {
-  return get('/api/travelogue/list')
-}
+export const getTravelogs = (data) => {
+  const { page, limit, travelogueStatus, keyword } = data;
+  // 构建基础的 URL
+  let url = `/api/travelogue/list?page=${page}&limit=${limit}`;
+  // 如果传入了 travelogueStatus，加入 URL 中
+  if (travelogueStatus !== null) {
+    url += `&travelogueStatus=${travelogueStatus}`;
+  }
+  // 如果传入了 keyword，加入 URL 中
+  if (keyword) {
+    url += `&keyword=${keyword}`;
+  }
+  // 返回 get 请求
+  return get(url);
+};
 
 // 搜索游记
 export const searchTravelogs = (keyword) => {
--- myApp/src/pages/index/index.vue
+++ myApp/src/pages/index/index.vue
@@ -3,48 +3,80 @@
     <view>
       <view>
         <AtSearchBar @clear="onClear" v-model:value="searchQuery" placeholder="搜索游记标题或作者昵称"
-          @action-click="debouncedSearch" />
+                     @action-click="debouncedSearch" />
       </view>
     </view>
     <view class="waterfall-container">
-      <view v-for="(item, index) in travelCards" :key="index" class="waterfall-item">
-        <image :src="item.travelogueCover" class="item-image" mode="aspectFill" />
+      <view v-for="(item, index) in travelCards" :key="index" class="waterfall-item"
+            @tap="gotoDetail(item.travelogueId)">
+        <image :src="item.travelogueCover" class="item-image"   mode="widthFix" />
         <view class="item-content">
           <text class="item-title">{{ item.travelogueTitle }}</text>
           <view class="user-info">
-            <AtAvatar circle size="small" :image="item.authorAvatar || defaultAvatar" />
+            <image class="avatar" :src="item.authorAvatar || defaultAvatar" />
             <text class="user-name">{{ item.travelogueAuthor }}</text>
           </view>
-          <!-- 由于不知名原因，在view 标签中添加 @click 事件，不起作用。 -->
-          <AtButton type='primary' @click="gotoDetail(item.travelogueId)">跳转按钮</AtButton>
         </view>
       </view>
     </view>
     <view v-if="loading" class="loading">加载中...</view>
+    <view v-if="!hasMore" class="loading">没有更多数据了</view>
   </view>
+
 </template>
 
 <script setup>
 import './index.scss';
 import { ref } from 'vue';
-import { AtAvatar, AtSearchBar,AtButton } from 'taro-ui-vue3';
-import Taro from '@tarojs/taro';
+import { AtSearchBar } from 'taro-ui-vue3';
+import Taro, { usePullDownRefresh, useReachBottom } from '@tarojs/taro';
 import { getTravelogs, searchTravelogs } from '../../api/travelogue.js'
 
 const travelCards = ref([]); // 存储游记卡片数据
 const searchQuery = ref(''); // 搜索查询
 const loading = ref(false); // 加载状态
+let page = 1; // 当前页数
+const hasMore = ref(true); // 是否还有更多数据
 const defaultAvatar = 'https://img.soogif.com/esrLXK1tXYDebZBHAKgmGx58EZd1smzH.jpeg_s400x0';
 
-// 模拟分页加载函数
-const loadTravelCards = async () => {
-  // 这里应该替换为实际的 API 调用
-  const res = await getTravelogs()
-  console.log(res.data.items)
-  const data = res.data.items; // 假设从 API 获取的数据
-  travelCards.value.push(...data);
+// 分页查询函数
+const loadTravelCards = async (isRefresh = false) => {
+  if (loading.value || !hasMore.value && !isRefresh) return;
+
+  loading.value = true;
+  if (isRefresh) {
+    page = 1;
+    hasMore.value = true;
+  }
+
+  const res = await getTravelogs({
+    page: page,
+    limit: 10
+  });
+
+  const data = res.data.items;
+
+  if (isRefresh) {
+    travelCards.value = data;
+  } else {
+    travelCards.value.push(...data);
+  }
+
+  // 判断是否还有更多
+  if (data.length < 10) {
+    hasMore.value = false;
+  } else {
+    page++;
+  }
+
   loading.value = false;
+
+  // 结束下拉刷新动画
+  if (isRefresh) {
+    Taro.stopPullDownRefresh();
+  }
 };
+
 
 // 防抖函数
 const debounce = (func, delay) => {
@@ -97,6 +129,19 @@ const gotoDetail = (travelogueId) => {
     url: `/pages/travelDetail/index?id=${travelogueId}`
   });
 };
+
+// 下拉刷新
+usePullDownRefresh(() => {
+  console.log('下拉刷新');
+  loadTravelCards(true);
+});
+
+
+// 触底更多
+useReachBottom(() => {
+  console.log('触底加载更多');
+  loadTravelCards();
+});
 
 // 页面加载时获取数据
 loadTravelCards();
