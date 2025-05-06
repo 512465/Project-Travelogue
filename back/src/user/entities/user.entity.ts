2c3876c0fa50783ffffa5aba1499b08de5f62402
王林 <1570898673@qq.com>
2025-05-06 18:49:01 +0800

    feat(user): 更新用户头像功能并优化个人中心页面
    - 在 UserEntity 中将用户头像默认值改为为空字符串
    - 新增用户头像更新接口和服务方法
    - 开发小程序端更新头像功能，包括选择图片和上传至服务器
    - 优化个人中心页面布局和样式，增加用户信息展示和功能按钮
    - 移除计数器组件和相关 store
    - 更新请求工具，添加 PATCH 方法支持

--- back/src/user/entities/user.entity.ts
+++ back/src/user/entities/user.entity.ts
@@ -33,7 +33,7 @@ export class UserEntity {
   })
   userPassword: string; // 用户密码，非空
 
-  @Column({ default: 'uploads/1.png' })
+  @Column({ default: '' })
   userAvatar: string; // 用户头像，非空
 
   // 用户与游记的关系，一个用户可以有多个游记
