02ad080ca6de5f242c6877bf75b338dfaab256e2
HG-abab <1695059005@qq.com>
2025-05-07 22:51:27 +0800

    feat:更换元服务器地址

--- myApp/src/pages/favorites/index.vue
+++ myApp/src/pages/favorites/index.vue
@@ -34,7 +34,7 @@ const userStore = useUserStore()
 const getTravelogs = async () => {
   try {
     const res = await Taro.request({
-      url: 'https://travle.hub.feashow.cn/api/travelogue/userCollects',
+      url: 'http://175.24.138.67:8586/api/travelogue/userCollects',
       method: 'GET',
       header: {
         Authorization: `Bearer ${userStore.token}`
